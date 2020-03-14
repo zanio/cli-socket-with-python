dc7a3707dfff8af1fe1a0573b3cb27ca709db22b
ANIEFIOK AKPAN <akp.ani@yahoo.com>
2020-03-14 16:19:25 +0100

    chore(add-host):add host

--- client.py
+++ client.py
@@ -1,19 +1,19 @@
 import socket
 
 def client_program():
-    host = '172.16.0.201'  # as both code is running on same pc
+    host = socket.gethostname()  # as both code is running on same pc
     port = 5000  # socket server port number
 
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
 
     message = input(" -> ")  # take input
-
     while message.lower().strip() != 'bye':
         client_socket.send(message.encode())  # send message
         data = client_socket.recv(1024).decode()  # receive response
 
         print('Received from server: ' + data)  # show in terminal
+        print("connection from the client:"+host)
 
         message = input(" -> ")  # again take input
 
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 
 def server_program():
     # get the hostname
-    host = '0.0.0.0'
+    host = socket.gethostname()
     port = 5000  # initiate port no above 1024
 
     server_socket = socket.socket()  # get instance
